c85b087d792f39fdd8424e629e70a97e2002cf2b
Juan Gonon <juanfegonon27@gmail.com>
2024-08-04 16:04:14 -0600

    cron

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,8 +1,23 @@
+import { CronJob } from "cron"
+
+
 
 export class Server{
 
     public static start(){
         
         console.log('Server started...')
+
+        const job = new CronJob(
+            '*/2 * * * * *', // cronTime
+             () => {
+                const date = new Date()
+                console.log('2 second', date);
+            }
+        );
+
+        job.start()
+
+
     }
 }
