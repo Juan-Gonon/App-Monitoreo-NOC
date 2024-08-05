ce81bc127b621e38a2341410fd8bc59f86293469
Juan Gonon <juanfegonon27@gmail.com>
2024-08-04 19:12:29 -0600

    service google

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -11,6 +11,8 @@ export class CheckService implements CheckServiceUseCase {
 
             if(!req.ok) throw new Error(` Error on check service ${url} `)
 
+            
+            console.log(`${url} is ok`)
              return true
             
         } catch (error) {
--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,3 +1,4 @@
+import { CheckService } from "../domain/use-cases/checks/check-service"
 import { CronService } from "./cron/cron-service"
 
 
@@ -13,8 +14,8 @@ export class Server{
         const job = CronService.createJob(
             '*/5 * * * * *',
             () => {
-                const date = new Date();
-                console.log('5 seconds ', date)
+                
+                new CheckService().execute('http://google.com')
             }
         )
 
