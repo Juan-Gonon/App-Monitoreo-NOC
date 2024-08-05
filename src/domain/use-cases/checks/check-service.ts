1205edcbd743286aba7c9f7f86111601c3369292
Juan Gonon <juanfegonon27@gmail.com>
2024-08-04 19:08:52 -0600

    useCase

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,28 @@
+
+interface CheckServiceUseCase{
+    execute(url: string) : Promise<boolean>
+}
+
+export class CheckService implements CheckServiceUseCase {
+
+    async execute ( url: string):Promise<boolean>{
+        try {
+            const req = await fetch(url)
+
+            if(!req.ok) throw new Error(` Error on check service ${url} `)
+
+             return true
+            
+        } catch (error) {
+
+            console.log(`${error}`);
+            
+            
+
+            return false
+        }
+
+
+        
+    }
+}
