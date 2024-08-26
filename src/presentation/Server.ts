38ac541d8e15755a744b511149af297b4f785604
Juan Gonon <juanfegonon27@gmail.com>
2024-08-26 10:49:57 -0600

    get from mongo

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,3 +1,4 @@
+import { logSeverityLevel } from "../domain/entities/log.entities"
 import { CheckService } from "../domain/use-cases/checks/check-service"
 import { SendEmailLogs } from "../domain/use-cases/email/send-logs"
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource"
@@ -17,7 +18,7 @@ const emailService = new EmailService()
 
 export class Server{
 
-    public static start(){
+    public static async start(){
         
         console.log('Server started...')
 
@@ -28,22 +29,25 @@ export class Server{
 
      
         // emailService.sendEmailWithFileSystemLogs([ 'jgonon549@gmail.com', 'juanfegonon27@gmail.com' ])
+
+        const logs = await logRepository.getLogs(logSeverityLevel.high)
+        console.log(logs)
    
-        const job = CronService.createJob(
-            '*/5 * * * * *',
-            () => {
+        // const job = CronService.createJob(
+        //     '*/5 * * * * *',
+        //     () => {
 
-                const url = 'http://google.com'
+        //         const url = 'http://google.com'
                 
-                new CheckService(
-                    logRepository,
-                    () => console.log(`${url} is ok`),
-                    (error) => console.log(error)
-                ).execute(url)
-            }
-        )
+        //         new CheckService(
+        //             logRepository,
+        //             () => console.log(`${url} is ok`),
+        //             (error) => console.log(error)
+        //         ).execute(url)
+        //     }
+        // )
 
-        job.start()
+        // job.start()
 
 
     }
