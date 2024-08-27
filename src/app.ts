1f2e16abc8efe132350930d2aa814b58b44d2c2b
Juan Gonon <juanfegonon27@gmail.com>
2024-08-26 22:28:36 -0600

    inset in postgres

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client"
 import { envs } from "./config/plugins/envs.plugin"
 import { LogModel, MongoDatabase } from "./data"
 import { Server } from "./presentation/Server"
@@ -19,6 +20,18 @@ async function main(){
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME
     })
+
+    const prisma = new PrismaClient()
+
+    const newLog = await prisma.logModel.create({
+        data: {
+            level: 'HIG',
+            message: 'Test message',
+            origin: 'App.ts'
+        }
+    })
+
+    console.log(newLog)
 
     Server.start()
 }
